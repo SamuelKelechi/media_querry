8f34e6c45ffd2c5798b3b9443612ebb0c599480d
SamuelKelechi <kcfreshie@ymail.com>
2022-12-05 16:00:23 +0100

    Updated Profile

---
+++ src/Components/Home/Dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Dashboard.css'
+
+function Dashboard() {
+  return (
+    <>
+      <div className='Dash_Main'>
+          <div className='Dash_Contain'>
+              <div className='Profile'>hi</div>
+              <div className='Dashboard'>hi</div>
+          </div>
+          <div className='Circle1'></div>
+          <div className='Circle2'></div>
+      </div>  
+    </>
+  )
+}
+
+export default Dashboard
